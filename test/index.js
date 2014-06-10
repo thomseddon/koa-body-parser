ec44a764039a9529771c2baa2cf6f4cccba287c8
Thom Seddon <thom@seddonmedia.co.uk>
2014-06-10 10:10:56 +0100

    Allow double parsing

--- test/index.js
+++ test/index.js
@@ -39,7 +39,25 @@ describe('koa-body', function () {
       .expect(200, done);
   });
 
-  describe('empty body', function () {
+  it('should allow body to be parsed twice', function (done) {
+    var app = koa();
+
+    app.use(parseBody());
+    app.use(parseBody());
+
+    app.use(function *() {
+      this.request.body.should.eql({ hello: 'world' });;
+      this.status = 200;
+    });
+
+    request(app.listen())
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .send({ hello: 'world' })
+      .expect(200, done);
+  });
+
+  describe('with an empty body', function () {
     it('should not throw when GET', function (done) {
       var app = koa();
 
